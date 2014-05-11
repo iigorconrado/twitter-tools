96480897bcb9c2e67b75e200503ed60ba05d4af4
Igor Conrado Alves de Lima <iigorconrado@gmail.com>
2014-05-11 16:46:05 +1000

    Fix filtering of tweets by locations and languages; Add option of choosing not to filter location considering places' bounding boxes as sometimes the API also gets tweets from nearby instead of only giving tweets from the given bounding box

--- twitter-tools-core/src/main/java/cc/twittertools/stream/GatherStatusStream.java
+++ twitter-tools-core/src/main/java/cc/twittertools/stream/GatherStatusStream.java
@@ -19,6 +19,7 @@ package cc.twittertools.stream;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -30,7 +31,11 @@ import org.apache.log4j.rolling.RollingFileAppender;
 import org.apache.log4j.rolling.TimeBasedRollingPolicy;
 import org.apache.log4j.varia.LevelRangeFilter;
 
+import cc.twittertools.search.api.RunQueriesThrift;
 import twitter4j.FilterQuery;
+import twitter4j.JSONArray;
+import twitter4j.JSONException;
+import twitter4j.JSONObject;
 import twitter4j.RawStreamListener;
 import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
@@ -41,6 +46,7 @@ public final class GatherStatusStream {
   
   private static final String LANGUAGE_OPTION = "language";
   private static final String LOCATIONS_OPTION = "locations";
+  private static final String NO_BOUNDING_BOX_OPTION = "no-bounding-box";
   
   @SuppressWarnings("unused")
   private static final String MINUTE_ROLL = ".%d{yyyy-MM-dd-HH-mm}.gz";
@@ -49,12 +55,17 @@ public final class GatherStatusStream {
   @SuppressWarnings("static-access")
   public static void main(String[] args) throws TwitterException {
     Options options = new Options();
-    
-    options.addOption(OptionBuilder.withArgName("list").hasArg()
-        .withDescription("comma-separated list of BCP 47 language identifiers").create(LANGUAGE_OPTION));
-    options.addOption(OptionBuilder.withArgName("list").hasArg()
+    options.addOption(OptionBuilder.withArgName("list")
+        .hasArgs()
+        .withDescription("comma-separated list of BCP 47 language identifiers")
+        .withLongOpt(LANGUAGE_OPTION)
+        .create('l'));
+    options.addOption(OptionBuilder.withArgName("list")
+        .hasArgs()
         .withDescription("comma-separated list of longitude,latitude pairs specifying a set of bounding boxes")
-        .create(LOCATIONS_OPTION));
+        .withLongOpt(LOCATIONS_OPTION)
+        .create('g'));
+    options.addOption("n", NO_BOUNDING_BOX_OPTION, false, "do not consider places' bounding box");
     
     CommandLine cmdline = null;
     CommandLineParser parser = new GnuParser();
@@ -62,6 +73,8 @@ public final class GatherStatusStream {
       cmdline = parser.parse(options, args);
     } catch (ParseException exp) {
       System.err.println("Error parsing command line: " + exp.getMessage());
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(RunQueriesThrift.class.getName(), options);
       System.exit(-1);
     }
     
@@ -111,14 +124,98 @@ public final class GatherStatusStream {
     rootLogger.addAppender(statusesAppender);
     rootLogger.addAppender(warningsAppender);
 
+    // creates filters for the query
+    FilterQuery fq = new FilterQuery();
+    StringBuilder criteria = new StringBuilder();
+    
+    /*
+     * @see https://dev.twitter.com/docs/streaming-apis/parameters#language
+     */
+    final boolean filterLanguage = cmdline.hasOption(LANGUAGE_OPTION);
+    String[] languages = null;
+    if (filterLanguage) {
+      languages = cmdline.getOptionValue(LANGUAGE_OPTION).split(",");
+      fq.language(languages);
+      criteria.append("languages: [" + cmdline.getOptionValue(LANGUAGE_OPTION) + "]\t");
+    }
+    final String[] langs = languages;
+    
+    /*
+     * @see https://dev.twitter.com/docs/streaming-apis/parameters#locations
+     */
+    double[][] locations = null;
+    if (cmdline.hasOption(LOCATIONS_OPTION)) {
+      String[] locationsArg = cmdline.getOptionValue(LOCATIONS_OPTION).split(",");
+      int nCoords = locationsArg.length; 
+      if (nCoords % 2 == 0) {
+        int pairs = nCoords/2;
+        locations = new double[pairs][2];
+        int cnt = 0;
+        for (int i=0; i<pairs;i++){
+          locations[i][0] = Double.parseDouble(locationsArg[cnt]);
+          cnt++;
+          locations[i][1] = Double.parseDouble(locationsArg[cnt]);
+          cnt++;
+        }
+        fq.locations(locations);
+        criteria.append("locations: [" + cmdline.getOptionValue(LOCATIONS_OPTION) + "]\t");
+      } else {
+        System.err.println("There is a missing coordinate. See "
+            + "https://dev.twitter.com/docs/streaming-apis/parameters#locations");
+        System.exit(-1);
+      }
+    } else {
+      fq.locations(new double[][] { { -180, -90 }, { 180, 90 } });
+    }
+    final double[][] loc = locations;
+    
+    final boolean no_bounding_box = cmdline.hasOption(NO_BOUNDING_BOX_OPTION);
+    if (no_bounding_box){
+      criteria.append("--no-bounding-box\t");
+    }
+    
     // creates a custom logger and log messages
     final Logger logger = Logger.getLogger(GatherStatusStream.class);
-
-    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+    
+    logger.info(criteria);
+    
     RawStreamListener rawListener = new RawStreamListener() {
 
       @Override
       public void onMessage(String rawString) {
+        if (no_bounding_box && loc != null){
+          try {
+            JSONObject status = new JSONObject(rawString);
+            JSONObject coordObj = status.getJSONObject("coordinates");
+            JSONArray coords = coordObj.getJSONArray("coordinates");
+            double longitude = coords.getDouble(0);
+            double latitude = coords.getDouble(1);
+            
+            // checks location
+            for (int i=0; i<loc.length;i+=2){
+              if (((loc[i][0] <= longitude) && (longitude <= loc[i+1][0])) ||
+                  ((loc[i][1] <= latitude) && (latitude <= loc[i+1][1]))){
+                break;
+              } else if (i == loc.length - 1) return;
+            }
+          } catch (JSONException e) { /* Either "Coordinates" is null or trash is coming*/
+            return;
+          }
+        }
+        
+        if (filterLanguage){
+          try {
+            JSONObject status = new JSONObject(rawString);
+            // checks language
+            String lang = status.getString("lang");
+            for (int i=0; i<langs.length; i++){
+              if (langs[i].equals(lang)) break;
+              else if (i == langs.length - 1) return;
+            }
+          } catch (JSONException e) { /* Trash is coming */
+            return;
+          }
+        }
         cnt++;
         logger.info(rawString);
         if (cnt % 1000 == 0) {
@@ -130,22 +227,9 @@ public final class GatherStatusStream {
       public void onException(Exception ex) {
         logger.warn(ex);
       }
-
     };
     
-    FilterQuery fq = new FilterQuery();
-    if (cmdline.hasOption(LANGUAGE_OPTION)) {
-      String[] languages = cmdline.getOptionValue(LANGUAGE_OPTION).split(",");
-      fq.language(languages);
-    }
-    if (cmdline.hasOption(LOCATIONS_OPTION)) {
-      String[] locations = cmdline.getOptionValue(LOCATIONS_OPTION).split(",");
-      double[][] loc = {{Double.parseDouble(locations[0]), Double.parseDouble(locations[1])},
-          {Double.parseDouble(locations[2]), Double.parseDouble(locations[3])}};
-      fq.locations(loc);
-    } else {
-      fq.locations(new double[][] { { -180, -90 }, { 180, 90 } });
-    }
+    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
     twitterStream.addListener(rawListener);
     twitterStream.filter(fq);
   }
